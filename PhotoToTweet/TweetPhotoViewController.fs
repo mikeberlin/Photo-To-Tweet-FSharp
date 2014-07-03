1452b55ee63d90e61faf0d4a446adf76b76f8c84
Mike Berlin <me@mike-berlin.com>
2014-07-03 16:58:53 -0500

    Added sepia filter functionality. Added tweeting capabilities.

--- PhotoToTweet/TweetPhotoViewController.fs
+++ PhotoToTweet/TweetPhotoViewController.fs
@@ -5,36 +5,48 @@ open System.Drawing
 
 open MonoTouch.UIKit
 open MonoTouch.Foundation
+open MonoTouch.CoreImage
+
+open Xamarin.Social
+open Xamarin.Social.Services
 
 [<Register ("TweetPhotoViewController")>]
-type TweetPhotoViewController (photo: UIImage) =
+type TweetPhotoViewController (image: UIImage) =
     inherit UIViewController ()
+
+    let originalImage = image
+    let originalOrientation = originalImage.Orientation
+
+    let sepiaFilter = 
+        let filter = new CISepiaTone()
+        filter.Image <- new CIImage(originalImage.CGImage)
+        filter.Intensity <- 0.8f
+        filter
+
+    let sepiaImage =
+        let ciContext = CIContext.FromOptions(null)
+        new UIImage(ciContext.CreateCGImage(sepiaFilter.OutputImage, sepiaFilter.Image.Extent))
 
     let mutable photoPreview =
         let p = new UIImageView(new RectangleF(22.0f, 75.0f, 275.0f, 400.0f))
-        p.Image <- photo
+        p.Image <- originalImage
         p
 
     let setFilter =
         let button = new UIButton(new RectangleF(22.0f, 486.0f, 125.0f, 44.0f))
         button.BackgroundColor <- UIColor.FromRGB(41, 128, 185)
         button.SetTitle("Toggle Filter", UIControlState.Normal)
+        photoPreview.Tag <- 0
 
         button.TouchUpInside.Add <| fun _ ->
-            button.BackgroundColor <- UIColor.White
-            // TODO: Insert sepia color toggle here...
-
-
-            (*
-                CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"
-                              keysAndValues: kCIInputImageKey, beginImage,
-                    @"inputIntensity", @0.8, nil];
-                CIImage *outputImage = [filter outputImage];
-                 
-                // 4
-                UIImage *newImage = [UIImage imageWithCIImage:outputImage];
-                self.imageView.image = newImage;  
-            *)
+            if (photoPreview.Tag = 0) then
+                photoPreview.Image <- new UIImage(sepiaImage.CGImage, 1.0f, originalOrientation)
+                photoPreview.Tag <- 1
+                button.BackgroundColor <- UIColor.Orange
+            else
+                photoPreview.Image <- new UIImage(originalImage.CGImage, 1.0f, originalOrientation)
+                photoPreview.Tag <- 0
+                button.BackgroundColor <- UIColor.FromRGB(41, 128, 185)
 
         button
 
@@ -42,38 +54,6 @@ type TweetPhotoViewController (photo: UIImage) =
         let button = new UIButton(new RectangleF(173.0f, 486.0f, 125.0f, 44.0f))
         button.BackgroundColor <- UIColor.FromRGB(41, 128, 185)
         button.SetTitle("Tweet Photo", UIControlState.Normal)
-
-        button.TouchUpInside.Add <| fun _ ->
-            button.BackgroundColor <- UIColor.Orange
-            // TODO: Insert built-in iOS tweet logic here...
-
-            (*
-            if (TWTweetComposeViewController.CanSendTweet) then
-                // Add code below
-            } else {
-                // Show a message: Twitter may not be configured in Settings
-            }
-
-            var tweet = new TWTweetComposeViewController();
-            tweet.SetInitialText ("Tweeting from my iOS app");
-
-            tweet.SetCompletionHandler((TWTweetComposeViewControllerResult r) =>{
-                DismissModalViewControllerAnimated(true); // hides the tweet
-                if (r == TWTweetComposeViewControllerResult.Cancelled) {
-                    // user cancelled the tweet
-                } else {
-                    // user sent the tweet (they may have edited it first)
-                }
-            });
-
-            PresentModalViewController(tweet, true);
-
-            tweet.AddUrl (new USUrl("http://xamarin.com"));
-
-            tweet.AddImage (UIImage.FromFile("some_image.png"));
-
-            *)
-
         button
 
     override this.ViewDidLoad() =
@@ -81,5 +61,19 @@ type TweetPhotoViewController (photo: UIImage) =
 
         this.View <- new UIView(BackgroundColor = UIColor.FromRGB(78, 87, 88))
         this.Title <- "Edit/Tweet Photo"
+
+        tweetPhoto.TouchUpInside.Add <| fun _ ->
+            let tweet = new Item()
+            tweet.Text <- "I'm tweeting using F#! #xamarin"
+            // add a link if you want to, so many options! =)
+            // tweet.Links.Add(new Uri("http://xamarin.com"))
+            tweet.Images.Add(new ImageData(photoPreview.Image))
+
+            let vcTwitter =
+                let twitterService = new Twitter5Service()
+                twitterService.GetShareUI(tweet, fun _ ->
+                    this.DismissViewController(true, null))
+
+            this.PresentViewController(vcTwitter, true, null)
 
         this.View.AddSubviews(photoPreview, setFilter, tweetPhoto)
